3ab2573bf8905976bfceffdcfc020afee026a438
pwh4862 <pwh4862@gmail.com>
2025-10-11 00:55:24 +0900

    게시

---
+++ src/algorithms/PotOdds_calculator.py
@@ -0,0 +1,13 @@
+def pot_odds(pot, call_amount):
+    """
+    pot: 현재 팟 금액
+    call_amount: 내가 콜해야 하는 금액
+    반환: 팟 오즈 퍼센트 (예: 16.67%)
+
+    """
+    odds = call_amount / (pot + call_amount)        # 소수점 4자리까지 내부 계산
+    percent = round(odds * 100, 2)                 # 퍼센트 변환 후 소수점 2자리 반올림
+    return f"{percent}%"
+
+
+print(pot_odds(100, 20))
